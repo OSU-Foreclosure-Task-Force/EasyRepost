6fb731c4c9721d5372a7afe8419260749e492af0
MiMiTooU <1975133679@qq.com>
2024-12-11 22:05:26 +0800

    adding TEMP_PATH for video temps it is init as a Path object

--- config.py
+++ config.py
@@ -1,11 +1,12 @@
 import configparser
 import secrets
-
+from pathlib import Path
 config = configparser.ConfigParser()
 config.read("config.ini")
 
 DEBUG = config["Dev"]["DEBUG"]
-SQLITE_URL = config["Database"]["SQLITE_URL"]
+SQLITE_URL = config["Database"]["SQLITE_PATH"]
 SUBSCRIPTION_TOKEN = secrets.token_hex()
 
 APP_TOKEN = config["Auth"]["APP_TOKEN"] if config["Auth"]["APP_TOKEN"] != "" else None
+TEMP_PATH = Path(config["Temp"]["TEMP_PATH"])
